a9b57815a1b0e5c8c81b832f1ce4b4e9e7f4023a
royJackman <jackman.roy@gmail.com>
2020-06-19 22:28:43 -0400

    Deprecate unused structs, replaced with ledger

--- src/main.rs
+++ src/main.rs
@@ -23,46 +23,6 @@ pub mod shop;
 pub mod purchase;
 pub mod util;
 
-#[derive(Serialize)]
-struct TemplateContext {
-    name: String,
-    items: Vec<&'static str>
-}
-
-#[derive(Serialize)]
-pub struct Market {
-    pub vendors: Mutex<Vec<shop::Vendor>>
-}
-
-impl Market {
-    pub fn new() -> Market {
-        Market{ vendors: Mutex::new(vec![]) }
-    }
-    
-    fn spawn_vendors(&mut self, count: usize) {
-        let mut rng = rand::thread_rng();
-        for i in 0..count {
-            let mut temp_vendor = shop::Vendor::new(format!("Vendor {}", i), format!("vendor_{}", i), rng.gen_range(700.0, 1300.0));
-            for j in 0..rng.gen_range(4,7) {
-                temp_vendor.items.push(shop::Item::new(
-                    format!("item {}", j), 
-                    rng.gen_range(1.0, 10.0), 
-                    rng.gen_range(30, 70)
-                ))
-            }
-            self.vendors.get_mut().unwrap().push(temp_vendor);
-        }
-    }
-
-    pub fn get_vendor(&mut self, name: &String) -> Option<&shop::Vendor> {
-        self.vendors.get_mut().unwrap().iter().find(|v| &v.name == name)
-    }
-    pub fn get_vendor_by_url(&mut self, url: &String) -> Option<&shop::Vendor> {
-        self.vendors.get_mut().unwrap().iter().find(|v| &v.url == url)
-    }
-}
-
-
 fn main() {
     let mut session_ledger = ledger::Ledger::new();
     let id = session_ledger.register_vendor("New Vendor".to_string(), None).unwrap_or("".to_string());
