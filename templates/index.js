a05faa57ad679479fa613fe01ef05c7aa3965baf
Roy <jackman.roy@gmail.com>
2020-07-06 20:56:07 -0400

    Resizable, switchable plots

--- templates/index.js
+++ templates/index.js
@@ -15,7 +15,7 @@ function renderGraph(type) {
     Plotly.newPlot('graph-canvas', data, layout);
 }
 
-function renderData(data) {
+function renderData(name, data) {
     var datum = [
         {
             line: { color: '#00F000'},
@@ -25,7 +25,7 @@ function renderData(data) {
         }
     ];
     var layout = {
-        title: 'test',
+        title: name,
         xaxis: {
             autotick: false,
             dtick: 1,
@@ -35,5 +35,21 @@ function renderData(data) {
             range: [0, 10]
         }
     };
-    Plotly.newPlot('graph-canvas', datum, layout);
+    var d3 = Plotly.d3;
+    var WIDTH_IN_PERCENT_OF_PARENT = 95,
+        HEIGHT_IN_PERCENT_OF_PARENT = 70;
+
+    var gd3 = d3.select("div[id='graph-canvas']")
+    .style({
+        width: WIDTH_IN_PERCENT_OF_PARENT + '%',
+        'margin-left': (100 - WIDTH_IN_PERCENT_OF_PARENT) / 2 + '%',
+        height: HEIGHT_IN_PERCENT_OF_PARENT + 'vh',
+        'margin-bottom': (100 - HEIGHT_IN_PERCENT_OF_PARENT) / 2 + 'vh'
+    });
+
+    var my_Div = gd3.node();
+            
+    Plotly.newPlot(my_Div, datum, layout);
+            
+    window.onresize = function() { Plotly.Plots.resize( my_Div ) };
 }
