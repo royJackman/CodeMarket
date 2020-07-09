e362404295a04e37871abc2d48b103a6a387e48d
Roy <jackman.roy@gmail.com>
2020-07-09 01:51:33 -0400

    main cleanup

--- src/main.rs
+++ src/main.rs
@@ -8,40 +8,61 @@ extern crate nanoid;
 
 #[cfg(test)] mod tests;
 
-use std::fmt;
 use std::collections::{HashMap, BTreeMap};
+use std::fmt;
 use std::sync::{Arc, RwLock};
 
 use config::*;
 use rocket::Request;
-use rocket_contrib::templates::Template;
 use rocket_contrib::serve::StaticFiles;
+use rocket_contrib::templates::Template;
 
+mod authorization;
 mod base;
 mod ledger;
-mod authorization;
 mod tera_functions;
-pub mod shop;
+
 pub mod purchase;
+pub mod shop;
 pub mod util;
 
 lazy_static! {
     pub static ref CONFIG: RwLock<Config> = RwLock::new({
-        let mut settings = Config::default();
-        settings.merge(File::with_name("Config.toml")).unwrap();
-        settings
+        let mut options = Config::default();
+        options.merge(File::with_name("Config.toml")).unwrap();
+        options
     });
 }
 
+/// Rust market launching point. This function starts the application, default
+/// port 8000. All settings can be changed using `Config.toml`
+/// 
+/// # Config options
+/// 
+/// * `generated_vendors`   - Number of AI vendors to generate
+#[allow(unused_variables, unused_assignments)]
 fn main() {
     let mut session_ledger = ledger::Ledger::new();
     let mut ids = vec![];
-    ids.push(session_ledger.register_vendor(util::name_generator(), None).unwrap_or("".to_string()));
-    ids.push(session_ledger.register_vendor(util::name_generator(), Some("oldies".to_string())).unwrap_or("".to_string()));
-    ids.push(session_ledger.register_vendor(util::name_generator(), None).unwrap_or("".to_string()));
-    ids.push(session_ledger.register_vendor(util::name_generator(), Some("icees".to_string())).unwrap_or("".to_string()));
+    match CONFIG.read().unwrap().get::<usize>("generated_vendors") {
+        Ok(gv) => {
+            for mut i in 0..gv {
+                match session_ledger.register_vendor(util::name_generator(), None) {
+                    Ok(id) => { ids.push(id); }, Err(_) => { i -= 1; }
+                }
+            }
+        },
+        Err(_) => {
+            ids.push(session_ledger.register_vendor(util::name_generator(), None).unwrap_or("".to_string()));
+            ids.push(session_ledger.register_vendor(util::name_generator(), Some("oldies".to_string())).unwrap_or("".to_string()));
+            ids.push(session_ledger.register_vendor(util::name_generator(), None).unwrap_or("".to_string()));
+            ids.push(session_ledger.register_vendor(util::name_generator(), Some("icees".to_string())).unwrap_or("".to_string()));
+        }
+    }
+
     println!("{:#?}", ids);
     session_ledger.show_avg_prices();
+    
     rocket::ignite()
            .manage( ledger::MutLedger{session_ledger: Arc::new(RwLock::new(session_ledger))} )
            .mount("/", StaticFiles::from("templates"))
