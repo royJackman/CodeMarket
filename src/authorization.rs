f71a90857003b497db2ef372b474e2506b522f91
Roy <jackman.roy@gmail.com>
2020-07-13 01:15:38 -0400

    Convert registration to form-urlencoded

--- src/authorization.rs
+++ src/authorization.rs
@@ -1,82 +1,14 @@
 use std::collections::BTreeMap;
 use std::fmt::Display;
-use std::io::{Read, self};
 
-use rocket::{Data, Outcome::*, Request, State};
-use rocket::data::{FromData, Outcome, Transform, Transformed};
-use rocket::http::Status;
 use rocket::response::content;
+use rocket::request::{Form, FormError};
+use rocket::State;
 
-const BUFFER_SIZE: u64 = 256;
-
-pub enum AuthorizationError {
-    ExistingVendor,
-    Io(io::Error)
-}
-
-#[derive(Debug)]
+#[derive(Debug, FromForm)]
 pub struct Registration {
     pub vendor_name: String,
     pub vendor_url: String
-}
-
-//Implement registration conversion
-impl Registration {
-    pub fn from_data(data: RegistrationData) -> Registration {
-        let clean_string = |string: &str| String::from(string).replace("\"", "").replace(",", "").replace("\r", "");
-        Registration {
-            vendor_name: clean_string(data.vendor_name),
-            vendor_url: clean_string(data.vendor_url)
-        }
-    }
-}
-
-//Registration DTO
-#[derive(Deserialize, Debug)]
-pub struct RegistrationData<'a> {
-    pub vendor_name: &'a str,
-    pub vendor_url: &'a str
-}
-
-//Convert to and from JSON
-impl<'a> FromData<'a> for RegistrationData<'a> {
-    type Error = AuthorizationError;
-    type Owned = String;
-    type Borrowed = str;
-
-    fn transform(_: &Request, data: Data) -> Transform<Outcome<Self::Owned, Self::Error>> {
-        let mut stream = data.open().take(BUFFER_SIZE);
-        let mut string = String::with_capacity((BUFFER_SIZE/2) as usize);
-        let outcome = match stream.read_to_string(&mut string) {
-            Ok(_) => Success(string),
-            Err(e) => Failure((Status::InternalServerError, Self::Error::Io(e)))
-        };
-
-        Transform::Borrowed(outcome)
-    }
-
-    fn from_data(_: &Request, outcome: Transformed<'a, Self>) -> Outcome<Self, Self::Error> {
-        let string = outcome.borrowed()?;
-        let tabboo: Vec<&str> = vec![&r"{", &r"}"];
-        let whitespace: Vec<&str> = vec![&"", &"\n"];
-        let splits: Vec<&str> = string.trim()
-                                      .split("\n")
-                                      .filter(move |x| {
-                                          for t in tabboo.iter() {
-                                              if x.contains(t) { return false }
-                                          }
-                                          for w in whitespace.iter() {
-                                              if x == w { return false }
-                                          }
-                                          true
-                                      })
-                                      .collect();
-        let to_remove: &[_] = &[',', '\n'];
-        Success(RegistrationData{
-            vendor_name: &splits[0].trim().trim_matches(to_remove).split("\"").filter(|x| x != &"").collect::<Vec<&str>>().last().unwrap(),
-            vendor_url: &splits[1].trim().trim_matches(to_remove).split("\"").filter(|x| x != &"").collect::<Vec<&str>>().last().unwrap()
-        })
-    }
 }
 
 /// The endpoint for registering new vendors using a JSON object. When a valid
@@ -87,10 +19,18 @@ impl<'a> FromData<'a> for RegistrationData<'a> {
 /// 
 /// * `registration_data`   - JSON object with registration info
 /// * `ledger`              - The current ledger state
-#[post("/register", format="application/json", data="<registration_data>")]
-pub fn register(registration_data: RegistrationData, ledger: State<super::ledger::MutLedger>) -> content::Json<String> {
+#[post("/register", data="<registration>")]
+pub fn register(registration: Result<Form<Registration>, FormError<'_>>, ledger: State<super::ledger::MutLedger>) -> content::Json<String> {
     let mut output_vars: BTreeMap<String, Box<dyn Display>> = BTreeMap::new();
-    let registration = Registration::from_data(registration_data);
+
+    let registration = match registration {
+        Ok(r) => r.into_inner(),
+        Err(_) => {
+            output_vars.insert("Registration".to_string(), Box::new("incorrectly formatted".to_string()));
+            return super::util::construct_json(&output_vars)
+        }
+    };
+
     if registration.vendor_name == "".to_string() {
         output_vars.insert("vendor_name".to_string(), Box::new("is empty".to_string()));
         return super::util::construct_json(&output_vars)
