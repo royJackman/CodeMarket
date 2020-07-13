8a66887571c55b29ceb0560110fe89895c2e7895
Roy <jackman.roy@gmail.com>
2020-07-12 22:42:26 -0400

    Tera functions cleanup

--- src/tera_functions.rs
+++ src/tera_functions.rs
@@ -1,13 +1,19 @@
-use super::util;
-use std::collections::BTreeMap;
 use rocket_contrib::templates::tera::{GlobalFn, Value, Error, from_value, to_value};
+use std::collections::BTreeMap;
+use super::util;
 
+/// Tera function for generating a catchphrase for the individual vendor pages
 pub fn make_catchphrase_generator() -> GlobalFn {
     Box::new(move |_args| -> Result<Value, Error> {
         Ok(to_value(super::util::catchphrase_generator()).unwrap())
     })
 }
 
+/// Tera function to find the index of a type in the static constant list
+/// 
+/// # Arguments
+/// 
+/// * `_data`   - Placeholder for data
 pub fn make_get_rust_type_index(_data: BTreeMap<String, String>) -> GlobalFn {
     Box::new(move |args| -> Result<Value, Error> {
         match args.get("data") {
@@ -20,6 +26,11 @@ pub fn make_get_rust_type_index(_data: BTreeMap<String, String>) -> GlobalFn {
     })
 }
 
+/// Tera function to parse an integer
+/// 
+/// # Arguments
+/// 
+/// * `_num`    - Placeholder for data
 pub fn make_intparse(_num: BTreeMap<String, String>) -> GlobalFn {
     Box::new(move |args| -> Result<Value, Error> {
         match args.get("num") {
